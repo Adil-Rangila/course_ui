0c865fb1dacfdfa8d6a1ceda6fb9c879b800cb9b
Adil Ahmed <adil.rangila@gmail.com>
2020-07-07 15:47:05 +0500

    inital setup with icons picture and top bar

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Course UI',
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.only(left: 20.0, top: 50.0, right: 20.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                SvgPicture.asset('assets/icons/menu.svg'),
+                Image.asset('assets/images/user.png'),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
